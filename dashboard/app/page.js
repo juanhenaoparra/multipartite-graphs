5ea3ba7afb562df21d5842ec051d3662e819d1f2
Juan <jhenao@truora.com>
2024-02-13 17:40:03 -0500

    create first draft of functional next.js project

--- dashboard/app/page.js
+++ dashboard/app/page.js
@@ -1,17 +1,11 @@
-import { useState } from 'react';
- 
+import LikeButton from './like-button';
+
 function Header({ title }) {
   return <h1>{title ? title : 'Default title'}</h1>;
 }
  
-function HomePage() {
+export default function HomePage() {
   const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
- 
-  const [likes, setLikes] = useState(0);
- 
-  function handleClick() {
-    setLikes(likes + 1);
-  }
  
   return (
     <div>
@@ -21,8 +15,8 @@ function HomePage() {
           <li key={name}>{name}</li>
         ))}
       </ul>
- 
-      <button onClick={handleClick}>Like ({likes})</button>
+
+      <LikeButton />
     </div>
   );
 }
