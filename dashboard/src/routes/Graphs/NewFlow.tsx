f69ac0cd0f37b55b70819eed1c82d1bf7ed97cdd
Juan <jhenao@truora.com>
2024-03-16 08:13:02 -0500

    Support creating a random node in the new view

--- dashboard/src/routes/Graphs/NewFlow.tsx
+++ dashboard/src/routes/Graphs/NewFlow.tsx
@@ -1,6 +1,7 @@
 import { GenRandomHex } from "@/shared/id"
 import React, { useCallback, useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
+import { GenRandomFlow, GenRandomFlowInput } from "./api/api"
 
 const graphTypes = [
   "bipartite",
@@ -24,12 +25,34 @@ function capitalize(string: string) {
 export default function NewFlow({}) {
   const navigate = useNavigate()
   const [random, setRandom] = useState(false)
+  const [nodesNumber, setNodesNumber] = useState(100)
+  const [graphType, setGraphType] = useState("")
+  const [graphDirection, setGraphDirection] = useState("")
+  const [isWeighted, setIsWeighted] = useState(false)
+  const [isConnected, setIsConnected] = useState(false)
+  const [isComplete, setIsComplete] = useState(false)
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
     if (random) {
-      alert('Random flow not implemented yet')
+      const input: GenRandomFlowInput = {
+        nodesNumber,
+        graphType,
+        direction: graphDirection,
+        weighted: isWeighted,
+        connected: isConnected,
+        complete: isComplete,
+        probability: 0.5,
+        degree: 2,
+      }
+
+      GenRandomFlow(input).then((flow) => {
+        navigate(`/graphs/${flow.name}`)
+      }).catch((error) => {
+        alert("Error generating random graph, please check the api connection: " + error)
+      })
+
       return
     }
 
@@ -53,12 +76,23 @@ export default function NewFlow({}) {
           </div>
           { random && <div>
             <label htmlFor="num-nodes-number">Number of nodes</label>
-            <input type="number" name="num-nodes-number" defaultValue="100"/>
+            <input
+              type="number"
+              name="num-nodes-number"
+              defaultValue="100"
+              value={nodesNumber}
+              onChange={(e) => setNodesNumber(parseInt(e.target.value))}
+            />
           </div>}
           { random &&
           <div>
             <label htmlFor="slc-graph-type">Graph Type</label>
-            <select name="slc-graph-type" defaultValue="">
+            <select
+              name="slc-graph-type"
+              defaultValue=""
+              value={graphType}
+              onChange={(e) => setGraphType(e.target.value)}
+            >
             <option value="">Select one</option>
               {
                 graphTypes.map((type) => (
@@ -69,7 +103,12 @@ export default function NewFlow({}) {
           </div>}
           { random && <div>
             <label htmlFor="slc-graph-direction">Graph Direction</label>
-            <select name="slc-graph-direction" defaultValue="">
+            <select
+              name="slc-graph-direction"
+              defaultValue=""
+              value={graphDirection}
+              onChange={(e) => setGraphDirection(e.target.value)}
+            >
               <option value="">Select one</option>
               {
                 graphDirections.map((direction) => (
@@ -80,15 +119,30 @@ export default function NewFlow({}) {
           </div>}
           { random && <div>
               <label htmlFor="is-weighted">Is Weighted</label>
-              <input type="checkbox" name="is-weighted" />
+              <input
+                type="checkbox"
+                name="is-weighted"
+                checked={isWeighted}
+                onChange={(e) => setIsWeighted(e.target.checked)}
+              />
           </div>}
           { random && <div>
               <label htmlFor="is-connected">Is Connected</label>
-              <input type="checkbox" name="is-connected" />
+              <input
+                type="checkbox"
+                name="is-connected"
+                checked={isConnected}
+                onChange={(e) => setIsConnected(e.target.checked)}
+              />
           </div>}
           { random && <div>
               <label htmlFor="is-complete">Is Complete</label>
-              <input type="checkbox" name="is-complete" />
+              <input
+                type="checkbox"
+                name="is-complete"
+                checked={isComplete}
+                onChange={(e) => setIsComplete(e.target.checked)}
+              />
           </div>}
 
           <button
--- dashboard/src/routes/Graphs/api/api.ts
+++ dashboard/src/routes/Graphs/api/api.ts
@@ -1,6 +1,17 @@
 import { API_HOST } from '@/shared/env';
 import axios from 'axios';
 
+export type GenRandomFlowInput = {
+  nodesNumber: number
+  graphType?: string
+  direction: string
+  weighted: boolean
+  connected: boolean
+  complete: boolean
+  probability: number
+  degree: number
+}
+
 export async function GetFlow(id: string): Promise<Graph> {
   const response = await axios.get(`${API_HOST}/api/graphs/${id}`);
   return response.data;
@@ -8,6 +19,12 @@ export async function GetFlow(id: string): Promise<Graph> {
 
 export async function SaveFlow(flow) {
   return await axios.post(`${API_HOST}/api/upload`, flow).then((response) => {
+    return response.data;
+  })
+}
+
+export async function GenRandomFlow(input: GenRandomFlowInput) {
+  return await axios.post(`${API_HOST}/api/new`, input).then((response) => {
     return response.data;
   })
 }
