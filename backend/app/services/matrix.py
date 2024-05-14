e96aefac2875614d3d01cfbf7614f59641866d7d
Juan Henao <jhenao@truora.com>
2024-05-14 17:11:47 -0500

    create function to do product tensor in a specific row given a cut index and a left side exponent position.
    this to be able to insert the new column in a desired position with the right combinations, this because the multiplication combinations are different when insert the least significant position or most significant one.

--- backend/app/services/matrix.py
+++ backend/app/services/matrix.py
@@ -2,6 +2,18 @@ from typing import List
 import numpy as np
 import math
 
+def get_binary_position(binary: str, mask=None, unmask=None):
+    if mask:
+        binary = "".join([binary[i] for i in range(len(binary)) if i in mask])
+        if binary == "":
+            return 0
+    elif unmask:
+        binary = "".join([binary[i] for i in range(len(binary)) if i not in unmask])
+        if binary == "":
+            return 0
+
+    return int(binary[::-1], 2)
+
 def product_tensor(matrix: np.ndarray, row: int = None):
     """
     Do product tensor of a matrix with m rows and n columns. Being n columns with 2 states.
@@ -39,6 +51,33 @@ def product_tensor(matrix: np.ndarray, row: int = None):
 
     return m_tensor
 
+def product_tensor_with_cut(matrix: np.ndarray, row: int, cut: int, left_side_exp: int):
+    """
+    Do product tensor of a matrix given a cut column. The cut column is the column where the matrix will be divided.
+
+    matrix: np.ndarray    -> matrix to do the product tensor
+    row: int              -> row to do the product tensor
+    cut: int              -> column to do the cut
+    left_side_exp: int    -> exponent of the left side of the cut, this is used to calculate the position of  the resulting tensor
+    """
+    m, n = matrix.shape
+    columns = cut * (n - cut)
+
+    vector_tensor = np.full((1, columns), np.nan)
+    tensors = round(math.log2(columns))
+
+    if m == 1:
+        row = 0
+
+    for i in range(columns):
+        full_bin = bin(i)[2:].zfill(tensors)[::-1]
+        left_index = get_binary_position(full_bin[left_side_exp])
+        right_index = get_binary_position(full_bin, unmask=[left_side_exp])
+
+        vector_tensor[0][i] = matrix[row][left_index] * matrix[row][cut+right_index]
+
+    return vector_tensor
+
 def get_emd(a, b):
     """
     Calculate the Earth Mover's Distance between two sets of points.
@@ -67,10 +106,10 @@ def marginalize(matrix: np.ndarray, tensors: int, positions: list, axis: int):
     Marginalize a matrix along an axis removing the position element.
 
     params:
-    @matrix: np.ndarray    -> matrix to marginalize
-    @position: List[int]   -> positions to remove
-    @axis: int             -> 0: row, 1: column
-    @significant_side: int -> 0: left, 1: right
+    matrix: np.ndarray    -> matrix to marginalize
+    tensors: int          -> number of tensors
+    position: List[int]   -> positions to remove
+    axis: int             -> 0: row, 1: column
     """
     m, n = matrix.shape
 
@@ -99,7 +138,6 @@ def marginalize(matrix: np.ndarray, tensors: int, positions: list, axis: int):
 
     max_size = m if axis == 1 else n
     iterator = {x for x in range(max_size)}
-
 
     while walked_rows < max_size:
         bottom = min(iterator)
