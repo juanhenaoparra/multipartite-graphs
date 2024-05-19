16967a3d00aede49ea27b039a2dc5f30f56ca72f
Felipe Giraldo <shiomediaz@gmail.com>
2024-05-19 14:38:48 -0500

    Se agregan las funcionalidades update_edge_weight y remove_edge a graphs.py

--- backend/app/schemas/graphs.py
+++ backend/app/schemas/graphs.py
@@ -54,18 +54,42 @@ class GraphNode(BaseModel):
 
         return key_exists and self.data[key] == value
 
+    def update_edge_weight(self, to_node_id: str, new_weight: float) -> bool:
+        for edge in self.linkedTo:
+            if edge.nodeId == to_node_id:
+                edge.weight = new_weight
+                return True
+        return False
 
+    def remove_edge_by_node_id(self, to_node_id: str) -> bool:
+        for edge in self.linkedTo:
+            if edge.nodeId == to_node_id:
+                self.linkedTo.remove(edge)
+                return True
+        return False
+    
 class GraphSchema(BaseModel):
     name: str
     data: List[GraphNode]
-    _nodesMap: Dict[str, GraphNode] = PrivateAttr(dict)
+    _nodesMap: Dict[str, GraphNode] = PrivateAttr(default_factory=dict)
 
     def set_nodes_map(self):
         self._nodesMap = {node.id: node for node in self.data}
 
-    def get_node_by_id(self, nodeId: str) -> GraphNode:
+    def get_node_by_id(self, nodeId: str) -> Optional[GraphNode]:
         return self._nodesMap.get(nodeId)
 
     def get_children(self, nodeId: str) -> List[GraphNode]:
         return [self._nodesMap.get(edge.nodeId) for edge in self._nodesMap.get(nodeId).linkedTo]
+    
+    def update_edge_weight(self, from_node_id: str, to_node_id: str, new_weight: float) -> bool:
+        from_node = self.get_node_by_id(from_node_id)
+        if from_node:
+            return from_node.update_edge_weight(to_node_id, new_weight)
+        return False
 
+    def remove_edge(self, from_node_id: str, to_node_id: str) -> bool:
+        from_node = self.get_node_by_id(from_node_id)
+        if from_node:
+            return from_node.remove_edge_by_node_id(to_node_id)
+        return False
